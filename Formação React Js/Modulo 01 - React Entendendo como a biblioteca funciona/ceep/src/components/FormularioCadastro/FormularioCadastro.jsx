d844645f64e168368e5b69aa46f91f82698e91e2
Alexandre Neves <alexandreneves98@outlook.com>
2022-02-18 16:53:00 -0300

    Modulo 01 - React Entendendo como a biblioteca funciona: Controlando o estado e a renderização

--- Formação React Js/Modulo 01 - React Entendendo como a biblioteca funciona/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ Formação React Js/Modulo 01 - React Entendendo como a biblioteca funciona/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,30 +3,45 @@ import "./estilo.css";
 class FormularioCadastro extends Component {
   
 
-  constructor()
+  constructor(props)
   {
-    super();
+    super(props);
     this.titulo = "";
+    this.texto = "";
   }
 
-  handleMundancaTitulo(event)
-  {
+  _handleMundancaTitulo(event){
+    event.stopPropagation();
     this.titulo = event.target.value;
+  }
+  _handleMundancaTexto(event){
+    event.stopPropagation();
+    this.texto = event.target.value;
+  }
+
+  _criarNota(event){
+    event.preventDefault();
+    event.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
   }
   
   render() {
     return (
-      <form className="form-cadastro ">
+      <form 
+        className="form-cadastro "
+        onSubmit={this._criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handleMundancaTitulo.bind(this)}
+          onChange={this._handleMundancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handleMundancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
