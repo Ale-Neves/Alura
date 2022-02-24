577e5711f36b5ca5ec093627b0df440ca481eed6
Alexandre Neves <alexandreneves98@outlook.com>
2022-02-24 20:55:48 -0300

    Modulo 04 - React Hooks, Contextos e boas práticas: Controlando Ciclo de vida.

--- Formação React Js/Modulo 04 - React Hooks, Contextos e boas práticas/projeto-formulario/src/components/FormularioCadastro/DadosEntraga.jsx
+++ Formação React Js/Modulo 04 - React Hooks, Contextos e boas práticas/projeto-formulario/src/components/FormularioCadastro/DadosEntraga.jsx
@@ -1,59 +1,89 @@
 import React from "react";
 import {Button,TextField} from "@material-ui/core/";
+import { useState } from "react";
 
-function DadosEntrega() {
-  return (
-    <form>
-        <TextField 
-            id="cep" 
-            label="CEP" 
-            type="number"         
-            variant="outlined"
-            margin="normal"
-        />
+function DadosEntrega({aoEnviar}) {
 
-        <TextField 
-            id="endereco" 
-            label="Endereço" 
-            type="text"        
-            variant="outlined"
-            margin="normal"
-            fullWidth
-        />
+    const [cep, setCep] = useState("");
+    const [endereco, setEndereco] = useState("");
+    const [numero, setNumero] = useState("");
+    const [estado, setEstado] = useState("");
+    const [cidade, setCidade] = useState("");
 
-        <TextField 
-            id="numero" 
-            label="Numero" 
-            type="number"        
-            variant="outlined"
-            margin="normal"
-        />
+    return (
+        <form onSubmit={(event)=>{
+            aoEnviar({cep,endereco,numero,estado,cidade})
+        }}>
+            <TextField
+                value={cep}  
+                onChange={(event)=>{
+                    setCep(event.target.value);
+                }} 
+                id="cep" 
+                label="CEP" 
+                type="number"         
+                variant="outlined"
+                margin="normal"
+            />
 
-        <TextField 
-            id="estado" 
-            label="Estado" 
-            type="text"        
-            variant="outlined"
-            margin="normal"
-        />
+            <TextField
+                value={endereco}
+                onChange={(event)=>{
+                    setEndereco(event.target.value);
+                }} 
+                id="endereco" 
+                label="Endereço" 
+                type="text"        
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
 
-        <TextField 
-            id="cidade" 
-            label="Cidade" 
-            type="text"        
-            variant="outlined"
-            margin="normal"
-        />
+            <TextField
+                value={numero}
+                onChange={(event)=>{
+                    setNumero(event.target.value);
+                }}  
+                id="numero" 
+                label="Numero" 
+                type="number"        
+                variant="outlined"
+                margin="normal"
+            />
 
-        <Button 
-            variant="contained" 
-            color="primary"
-            type="submit"
-            fullWidth
-            >FINALIZAR CADASTRO
-        </Button>
-    </form>
-  );
+            <TextField
+                value={estado}
+                onChange={(event)=>{
+                    setEstado(event.target.value);
+                }}  
+                id="estado" 
+                label="Estado" 
+                type="text"        
+                variant="outlined"
+                margin="normal"
+            />
+
+            <TextField
+                value={cidade}
+                onChange={(event)=>{
+                    setCidade(event.target.value);
+                }} 
+                id="cidade" 
+                label="Cidade" 
+                type="text"        
+                variant="outlined"
+                margin="normal"
+            />
+
+            <Button 
+                variant="contained" 
+                color="primary"
+                type="submit"
+                fullWidth
+                >FINALIZAR CADASTRO
+            </Button>
+        </form>
+    );
 }
 
 export default DadosEntrega;
--- Formação React Js/Modulo 04 - React Hooks, Contextos e boas práticas/projeto-formulario/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ Formação React Js/Modulo 04 - React Hooks, Contextos e boas práticas/projeto-formulario/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,21 +1,45 @@
+import { Stepper, Step, StepLabel, Typography } from "@material-ui/core";
 import React, {useState} from "react";
+import { useEffect } from "react";
 import DadosEntrega from "./DadosEntraga";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({aoEnviar, validaCPF}) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDados] = useState({});
   const formularios = [
-    <DadosUsuario aoEnviar={proximaEtapa}/>,
-    <DadosPessoais aoEnviar={proximaEtapa} validaCPF={validaCPF}/>,
-    <DadosEntrega/>
+    <DadosUsuario aoEnviar={colotaDados}/>,
+    <DadosPessoais aoEnviar={colotaDados} validaCPF={validaCPF}/>,
+    <DadosEntrega aoEnviar={colotaDados}/>,
+    <Typography variant="h5">Obrigado pelo Cadastro!</Typography>
   ];
+
+  useEffect(()=>{
+    if(etapaAtual === formularios.length-1)
+      aoEnviar(dadosColetados);
+  })
+
+  function colotaDados(dados){
+    setDados({...dadosColetados, ...dados})
+    proximaEtapa();
+  }
 
   function proximaEtapa(){
     setEtapaAtual(etapaAtual+1);
   }
+
+
   
-  return<>{formularios[etapaAtual]}</>
+  return<>
+    <Stepper activeStep={etapaAtual}>
+      <Step><StepLabel>Login</StepLabel></Step>
+      <Step><StepLabel>Pessoal</StepLabel></Step>
+      <Step><StepLabel>Entrega</StepLabel></Step>
+      <Step><StepLabel>Finalização</StepLabel></Step>
+    </Stepper>
+    {formularios[etapaAtual]}
+  </>
 }
 
 export default FormularioCadastro;
