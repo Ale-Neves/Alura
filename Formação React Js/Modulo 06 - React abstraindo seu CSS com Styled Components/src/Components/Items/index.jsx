595e8a1b29e111ff5b18c1518db4d30a38efba39
Alexandre Neves <alexandreneves98@outlook.com>
2022-04-21 15:26:55 -0300

    Modulo 06 - React abstraindo seu CSS com Styled Components: Aplicando troca de Tema.

--- Formação React Js/Modulo 06 - React abstraindo seu CSS com Styled Components/src/Components/Items/index.jsx
+++ Formação React Js/Modulo 06 - React abstraindo seu CSS com Styled Components/src/Components/Items/index.jsx
@@ -4,7 +4,7 @@ import Item from "../Item";
 import ImageFilter from "../ImageFilter";
 
 const Items = styled.div`
-    box-shadow: 4px 4px 20px 0px rgba(0,0,0,0.1);
+    box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
     margin: 2px 0;
     display: flex;
