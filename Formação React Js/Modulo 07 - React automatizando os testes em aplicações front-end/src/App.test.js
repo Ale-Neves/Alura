842b649f7348c0af60d7cbcc8c8abd1dd3bcb1e3
Alexandre Neves <alexandreneves98@outlook.com>
2022-04-22 15:11:48 -0300

    Finalizando Formação de React JS.

--- Formação React Js/Modulo 07 - React automatizando os testes em aplicações front-end/src/App.test.js
+++ Formação React Js/Modulo 07 - React automatizando os testes em aplicações front-end/src/App.test.js
@@ -1,33 +1,53 @@
 import React from 'react';
-import App, { calcularNovoSaldo } from './App'
-import { render, screen} from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
-describe('Componente pricipal', () => {
+import App, { calcularNovoSaldo } from './app';
 
-    describe('Quando eu abro o app do banco', () => {
-        test('A Label do nome do banco', () => {
-            render(<App/>);
-            expect(screen.getByText('ByteBank')).toBeInTheDocument();
-        })
-        test('A Label saldo', () => {
-            render(<App/>);
-            expect(screen.getByText('Saldo:')).toBeInTheDocument();
-        })
-        test('A Label do botão Realizar operação', () => {
-            render(<App/>);
-            expect(screen.getByText('Realizar operação')).toBeInTheDocument();
-        })
+describe('Componente principal', () => {
+  describe('Quando eu abro o app do banco', () => {
+    test('o nome é exibido', () => {
+      render(<App />);
+  
+      expect(screen.getByText('ByteBank')).toBeInTheDocument();
     })
-
-    describe('Quando eu realizo uma transação', () => {
-        test('Saque, o valor vai ser diminuido', () => {
-            
-            const valores = {
-                transacao: 'saque',
-                valor: 50
-            }
-            const novoSaldo = calcularNovoSaldo(valores, 150);
-            expect(novoSaldo).toBe(100);
-        });
+    it('o saldo é exibido', () => {
+      render(<App />);
+    
+      expect(screen.getByText('Saldo:')).toBeInTheDocument();
     })
+    it('o botão de realizar transação é exibido', () => {
+      render(<App />);
+  
+      expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+    })
+  })
+  describe('Quando eu realizo uma transação', () => {
+    it('que é um saque, o valor vai diminuir', () => {
+      const valores = {
+        transacao: 'saque',
+        valor: 50
+      }
+
+      const novoSaldo = calcularNovoSaldo(valores, 150)
+
+      expect(novoSaldo).toBe(100);
+    });
+    it('que é um saque, a transação deve ser realizada', () => {
+      render(<App />);
+
+      const saldo = screen.getByText('R$ 1000');
+      const transacao =  screen.getByLabelText('Saque');
+      const valor = screen.getByTestId('valor');
+      const botaoTransacao = screen.getByText('Realizar operação');
+
+      expect(saldo.textContent).toBe('R$ 1000')
+
+      fireEvent.click(transacao, { target: { value: 'saque'}});
+      fireEvent.change(valor, { target: { value:  10 }});
+      fireEvent.click(botaoTransacao);
+
+      expect(saldo.textContent).toBe('R$ 990');
+    })
+  })
 })
+
